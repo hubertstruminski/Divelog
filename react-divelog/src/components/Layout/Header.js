739bb94f247bb644db4845fe3b48237a5107502c
hubert.struminski <hubert.struminski@gmail.com>
2019-08-01 13:47:36 +0200

    add bootstrap styles to navigation in Header component

--- react-divelog/src/components/Layout/Header.js
+++ react-divelog/src/components/Layout/Header.js
@@ -8,55 +8,58 @@ class Header extends React.Component {
         const  { t, i18n } = this.props;
         return (
             <header>
-                <nav className="navbar navbar-expand-md navbar-dark bg-primary">
+                <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
                     <Link to="/home" className="navbar-brand">
                         <span className="logoSize">
                             Divelog
                         </span>
                     </Link>
-                    <div className="collapse navbar-collapse">
-                        <ul className="navbar-nav navbar-collapse justify-content-start">
-                            <li>
-                                <i 
+                    <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="navbar-collapse collapse" id="navbarToggleExternalContent">
+                       <ul className="navbar-nav mr-auto justify-content-start">
+                            <li className="nav-item active mt-2">
+                                <i
                                     onClick={() => i18n.changeLanguage("en")} 
-                                    className="gb uk flag" 
+                                    className="gb uk flag"
                                 />
                             </li>
-                            <li>
+                            <li className="nav-item active mt-2">
                                 <i 
                                     onClick={() => i18n.changeLanguage("de")} 
                                     className="de flag"
                                 />
                             </li>
-                            <li>
+                            <li className="nav-item active mt-2">
                                 <i 
                                     onClick={() => i18n.changeLanguage("pl")} 
                                     className="pl flag" 
                                 />
                             </li>
-                            <li>
+                            <li className="nav-item active">
                                 <Link to="/home" className="nav-link">
                                     {this.props.t("header.home")}
                                 </Link>
                             </li>
-                            <li>
+                            <li className="nav-item active">
                                 <Link to="/guide" className="nav-link">
                                     {this.props.t("header.guide")}
                                 </Link>
                             </li>
-                            <li>
+                            <li className="nav-item active">
                                 <Link to="/about" className="nav-link">
                                     {this.props.t("header.aboutMe")}
                                 </Link>
                             </li>
-                            <li>
+                            <li className="nav-item active">
                                 <Link to="/home" className="nav-link">
                                     {this.props.t("header.contact")}
                                 </Link>
                             </li>
-                        </ul>
-                        <ul className="navbar-nav navbar-collapse justify-content-end">
-                            <li>
+                       </ul>
+                       <ul className="navbar-nav my-lg justify-content-end">
+                            <li className="nav-item active">
                                 <Link to="/world_statistics" className="nav-link">
                                     Log In
                                 </Link>
