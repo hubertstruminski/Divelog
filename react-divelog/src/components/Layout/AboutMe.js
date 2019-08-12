e18137217ad3ca99497111426210ddac3166869a
hubert.struminski <hubert.struminski@gmail.com>
2019-08-12 23:12:19 +0200

    add responsive's for mobile devices in LogIn component

--- react-divelog/src/components/Layout/AboutMe.js
+++ react-divelog/src/components/Layout/AboutMe.js
@@ -7,31 +7,41 @@ class AboutMe extends React.Component {
     render() {
         return (
             <div className="about-container">
-                <div className="card card-size card-center card-shadow">
-                    <div className="grid-about-container">
-                        <div className="grid-about-item">
-                            <img src={me} alt="Hubert Strumiński" className="about-photo"/>
-                        </div>
-                        <div className="grid-about-item-2">
-                            <div className="about-item2">
-                                Hubert Strumiński
-                            </div>
-                            <div className="about-txt-1">
-                                <p>
-                                    {this.props.t("aboutMe.text")}
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="thanks-txt-2">
+                <div className="about-jumbotron about-jumbotron-shadow">
+                    <div className="about-jumbotron-text">
                         <p>
-                            {this.props.t("aboutMe.end-text")}
+                            {this.props.t("aboutMe.jumbotron")}
                         </p>
-                        <div className="about-greetings-txt-3">
+                    </div>
+                </div>
+
+                <div className="card-center card-margin-bottom">
+                    <div className="card card-size card-shadow">
+                        <div className="grid-about-container">
+                            <div className="grid-about-item">
+                                <img src={me} alt="Hubert Strumiński" className="about-photo"/>
+                            </div>
+                            <div className="grid-about-item-2">
+                                <div className="about-item2">
+                                    Hubert Strumiński
+                                </div>
+                                <div className="about-txt-1">
+                                    <p>
+                                        {this.props.t("aboutMe.text")}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="thanks-txt-2">
                             <p>
-                                {this.props.t("aboutMe.thanks")}
+                                {this.props.t("aboutMe.end-text")}
                             </p>
-                            <p className="signature-me">Hubert Strumiński</p>
+                            <div className="about-greetings-txt-3">
+                                <p>
+                                    {this.props.t("aboutMe.thanks")}
+                                </p>
+                                <p className="signature-me">Hubert Strumiński</p>
+                            </div>
                         </div>
                     </div>
                 </div>
